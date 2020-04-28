9d2c91c96978eff65bdd00fec5d8e19adb8a1f9d
Karen Berba <berbak@oregonstate.edu>
2020-04-27 18:58:50 -0700

    Edited About page

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,8 @@ export default () => (
         <h1>About Us</h1>
         <p>We're the OSU Hackathon Club -- all OSU students are welcome and encouraged to join!</p> 
         <p>Because our club was originally created with the needs of eCampus Post-Baccalaureate Computer Science students in mind, our monthly meetings, weekly HackerWeeks competitions, and our quarterly BeaverHacks hackathons are held entirely online.</p> 
-        <p>However, we also fund in-person events in locations wherever there is sufficient interest! And in the future, there are plans to create an on-campus group as well -- let us know if you are interested in helping develop that group!</p> 
-        <p>Through small, humble begins our club has continued to grow through the support of OSU students, alumni and faculty. We welcome tech enthusiasts and programmers of all skill levels and experiences! </p>
+        <p>However, we also fund in-person events in locations wherever there is sufficient interest! (Please contact our Local Chapter Coordinator for more information). And in the future, there are plans to create an on-campus group as well -- let us know if you are interested in helping develop the on-campus group or planning any in-person events!</p> 
+        <p>Through small, humble beginnings our club has continued to grow through the support of OSU students, alumni and faculty. We welcome tech enthusiasts and programmers of all skill levels and experiences! </p>
 
         <h2>Current Board</h2>
         <h3>President</h3><p>Jordan Bartos</p>
