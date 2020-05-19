9f8781c898e9e86c3e9ad1fa7e7f04c297437e94
Karen Berba <berbak@oregonstate.edu>
2020-05-19 13:33:37 -0700

    Fixed typo and added icon to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import "../assets/style.css";
 import Button from "react-bootstrap/Button";
+import logo from "../assets/img/osu_hackathon_club_logo_icon_right.png";
 
 
 export default () => (
@@ -11,6 +12,7 @@ export default () => (
         <Jumbotron>
             <h1>Oregon State University Hackathon Club</h1>
             <br />
+            <img src={logo} alt="OSU BeaverHacks mascot" />
             <p> Our mission is to strengthen the OSU community by fostering connections between students and building stronger programming skills through quarterly coding competitions and workshops. </p>
         </Jumbotron>
 
@@ -20,7 +22,7 @@ export default () => (
             <a href="https://osu-cs.slack.com/"><Button variant="orange" size="sm">Take on our challenges here!</Button></a>
             <br />
             <br />
-            <h4>BeaverHacks Summer 2020</h4><p>Register on Devpost by 11:59 pm on 6/15</p><a href="https://beaverhacks-summer-2020.devpost.com/"><Button varient="orange" size="sm">Devpost</Button></a>
+            <h4>BeaverHacks Summer 2020</h4><p>Register on Devpost by 11:59 pm on 6/15</p><a href="https://beaverhacks-summer-2020.devpost.com/"><Button variant="orange" size="sm">Devpost</Button></a>
         </div>
     </Layout>
 )
